81fcba6ee80a0064557aca3c013c6aecfc4fbcd1
ADDICT-HUB <hapawapi0@gmail.com>
2025-09-19 15:24:03 +0300

    Update tool-tempmail.js

--- plugins/tool-tempmail.js
+++ plugins/tool-tempmail.js
@@ -55,7 +55,7 @@ _Email will expire after 24 hours_
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363299029326322@newsletter',
+                        newsletterJid: '120363417996705218@newsletter',
                         newsletterName: 'TempMail Service',
                         serverMessageId: 101
                     }
